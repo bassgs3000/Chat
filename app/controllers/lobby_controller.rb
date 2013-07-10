53823e0043ce810ec9a1f16c1cc75d12dbfbfc2e
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-07-09 21:24:22 -0500

    Private Chatrooms are working a bit better now

--- app/controllers/lobby_controller.rb
+++ app/controllers/lobby_controller.rb
@@ -2,7 +2,7 @@ class LobbyController < ApplicationController
   def index
     if user_signed_in?
       @public_chatrooms = Chatroom.where(private: false)
-      @private_chatrooms = Chatroom.where(private: true, created_by: current_user.id)
+      @private_chatrooms = User.find(current_user.id).chatrooms.where(private: true)
     
       respond_to do |format|
         format.js
@@ -25,6 +25,7 @@ class LobbyController < ApplicationController
 
     respond_to do |format|
       if @chatroom.save
+        ChatroomMember.create!(user_id: current_user.id, chatroom_id: @chatroom.id, private: @chatroom.private)
         format.html { redirect_to lobby_path }
       else
         format.html { redirect_to lobby_path }
--- app/models/chatroom_member.rb
+++ app/models/chatroom_member.rb
@@ -1,5 +1,5 @@
 class ChatroomMember < ActiveRecord::Base
-  attr_accessible :chatroom_id, :user_id
+  attr_accessible :chatroom_id, :user_id, :private
   belongs_to :user
   belongs_to :chatroom
 end
